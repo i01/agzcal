4414f51428ddb8efcbe80952d49224c591203df7
Yuri <i01@users.noreply.github.com>
2026-05-31 13:22:08 +0000

    Upgrade to Nuxt v4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxt/ui'],
+  css: ["~/assets/css/main.css"],
   ui: {
     fonts: false
   },
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
-  css: ["~/assets/css/main.css"]
+  vite: {
+    optimizeDeps: {
+      include: [
+        '@fullcalendar/daygrid',
+        '@fullcalendar/interaction',
+        '@fullcalendar/timegrid',
+        '@fullcalendar/vue3',
+        '@paralleldrive/cuid2',
+        '@vue/devtools-core',
+        '@vue/devtools-kit',
+      ]
+    }
+  }
   /*
   nitro: {
     experimental: {
